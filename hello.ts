ac85325e9c483dcb623e3724d7629b8055d7e834
shalini1297 <shalini@sedintechnologies.com>
2023-11-08 12:38:55 +0530

    first commit

---
+++ hello.ts
@@ -0,0 +1,5 @@
+import { add , sayhelloto } from "./modules1"
+sayhelloto("shalini",25)
+sayhelloto("RANJITH",30)
+const addition = add(30,20)
+console.log(addition)
---
+++ modules1.ts
@@ -0,0 +1,8 @@
+export function sayhelloto(name:string, age: number){
+    console.log(`Hello ${name},your age ${age}`)
+
+}
+export function add(first:number, second :number){
+    return first +second ;
+
+}
