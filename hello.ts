89e539dc46b35ddb9b6c759ce24dba2de43e93b7
shalini1297 <shalini@sedintechnologies.com>
2023-11-14 20:46:27 +0530

    removed node modules from github and day4

--- hello.ts
+++ hello.ts
@@ -1,5 +1,62 @@
-import { add , sayhelloto } from "./modules1"
-sayhelloto("shalini",25)
-sayhelloto("RANJITH",30)
-const addition = add(30,20)
-console.log(addition)
+class Logger{
+    log <T>(message:String,additionalInfo:T){
+        console.log(`${message}${JSON.stringify(additionalInfo)}`)
+
+    }
+}
+const logger = new Logger();
+logger.log("this is error cause because of the information;",{name:"Shalini",Status: "Still learning"})
+logger.log("this is error cause because of the information;",{name:"Shalini"})
+/* type User={
+    id :number,
+    name:string
+}
+type Customer = User & {address :string}
+type Seller = User & {companyName : string}
+type Admin = User & {username :string}
+
+class Logger{
+    loguser <T extends User>(message:String,user:T){
+        console.log(`${message} for user with id ${user.id}and name ${user.name} }`)
+
+    }
+}
+const logger = new Logger();
+const customer :Customer ={
+    id:1,
+    name: 'shalini',
+    address:'shalini address'
+}
+    const seller : Seller={
+        id : 1,
+        name :'shalini',
+        companyName :'ABC'
+
+
+    }
+    const admin :Admin={
+        id :3,
+        name : 'sedin',
+        username :"Shalini@123"
+    }
+
+logger.loguser("this is error cause because of the information;", customer)
+logger.loguser("this is error cause because of the information;", seller)
+logger.loguser("this is error cause because of the information;", admin) 
+
+/* type Investenment= {  
+    investingcompany :string
+}
+
+type personal ={
+    category : string
+}
+
+type ExpenseRecord <T> ={
+    info:T,
+    amount :Number,
+    reason :string
+
+}
+const investenments :ExpenseRecord<Investenment>[] =[]
+const personalExpense : ExpenseRecord <personal>[] =[] */
--- modules1.ts
+++
@@ -1,8 +0,0 @@
-export function sayhelloto(name:string, age: number){
-    console.log(`Hello ${name},your age ${age}`)
-
-}
-export function add(first:number, second :number){
-    return first +second ;
-
-}
